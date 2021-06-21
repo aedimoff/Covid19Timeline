4220d3d06eb897febb21765aea776fbc8afdcb10
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-21 16:24:47 -0700

    add d3 map

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import { select } from 'd3';
+import { select, json, geoPath, geoMercator } from 'd3';
+import { feature } from 'topojson-client';
 
-const svg = select('svg');
-svg.style('background-color', 'red');
+const svg = select('svg')
+    .attr('width', 960)
+    .attr('height',1000)
+
+const path = geoPath()
+
+json('https://d3js.org/us-10m.v1.json')
+    .then( data => {
+        const states = feature(data, data.objects.states)
+        svg.selectAll('path')
+        .data(states.features)
+        .enter().append('path')
+            .attr('d', path);
+    });
