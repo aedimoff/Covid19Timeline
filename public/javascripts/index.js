472b86b005e5c80ab179fb8969e4e07d00520b9e
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-21 15:11:10 -0700

    add d3 library

--- public/javascripts/index.js
+++
@@ -1,5 +0,0 @@
-const axios = require('axios');
-
-document.addEventListener('DOMContentLoaded', () => {
-    //axios requests here
-})
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 const config = {
-  entry: path.join(__dirname, './public/javascripts/index.js'),
+  entry: path.join(__dirname, './src/index.js'),
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, './public')
