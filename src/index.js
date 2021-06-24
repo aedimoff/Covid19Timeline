41decb6a33c40ee11c900331b0a70ba5f051ccb7
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-24 11:02:01 -0700

    move render fuction

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import * as Map from './map';
-import axios from "axios";
 import './stylesheets/application.scss'
 import './stylesheets/map.scss'
 import './stylesheets/slider.scss'
+import {getDates, dateCodes} from './dates'
+import { renderMap } from './map'
+
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    let slider = document.getElementById('range-slider')
+    let output = document.getElementById('value')
+
+    output.innerHTML = getDates[slider.value];
+
+    let date = dateCodes[slider.value]
+    renderMap(date)
+
+    slider.oninput = function() {
+        output.innerHTML = getDates[this.value];
+        date = dateCodes[this.value]
+        renderMap(date)
+    }
+
+
+
 
 })
 
--- src/map.js
+++ src/map.js
@@ -1,84 +1,64 @@
 import { select, json, geoPath, tsv } from 'd3';
 import { feature } from 'topojson-client';
-import {getDates, dateCodes} from './dates'
-
-let slider = document.getElementById('range-slider')
-let output = document.getElementById('value')
-
-output.innerHTML = getDates[slider.value];
-
-let date = slider.value
-
-slider.oninput = function() {
-    output.innerHTML = getDates[this.value];
-    date = dateCodes[this.value]
-    console.log(date)
-}
 
 
+export const renderMap = (date) => {
+    const jsonUrl = 'https://gist.githubusercontent.com/aedimoff/43582253126b56f90b942f80eee13156/raw/statesnumeric.json'
 
-const jsonUrl = 'https://gist.githubusercontent.com/aedimoff/43582253126b56f90b942f80eee13156/raw/statesnumeric.json'
+    Promise.all([
+        json('https://d3js.org/us-10m.v1.json'),
+        json(jsonUrl)
+    ]).then(([topoJSONData, covidData]) => {
 
-Promise.all([
-    json('https://d3js.org/us-10m.v1.json'),
-    json(jsonUrl)
-]).then(([topoJSONData, covidData]) => {
+    function getStatsByMonth(selectedMonth, stateId) {
+        let stats;
 
-function getStatsByMonth(selectedMonth, stateId) {
-    let stats;
-
-    covidData.forEach(dataSet => {
-        for(let date in dataSet) {
-            if(date == selectedMonth) {
-                stats = dataSet[date][stateId]
+        covidData.forEach(dataSet => {
+            for(let date in dataSet) {
+                if(date == selectedMonth) {
+                    stats = dataSet[date][stateId]
+                }
             }
+        })
+        return stats;
+    }
+
+    function setColor(selectedMonth, stateId) {
+        let stats = getStatsByMonth(selectedMonth, stateId)
+        if(stats < 1000) {
+            return "lightgreen"
+        } else if(stats > 1000 && stats <= 5000) {
+            return "lightblue"
+        } else if(stats > 5000 && stats <= 10000) {
+            return "yellow" 
+        } else if (stats > 10000 && stats <= 30000) {
+            return "orange"
+        } else if (stats > 30000) {
+            return "red"
         }
-    })
-    return stats;
-}
-
-function setColor(selectedMonth, stateId) {
-    let stats = getStatsByMonth(selectedMonth, stateId)
-    if(stats < 1000) {
-        return "lightgreen"
-    } else if(stats > 1000 && stats <= 5000) {
-        return "lightblue"
-    } else if(stats > 5000 && stats <= 10000) {
-        return "yellow" 
-    } else if (stats > 10000 && stats <= 30000) {
-        return "orange"
-    } else if (stats > 30000) {
-        return "red"
+        
     }
-    
+
+    const states = feature(topoJSONData, topoJSONData.objects.states)
+    svg.selectAll('path').data(states.features)
+    .enter().append('path')
+        .attr('class', 'state')
+        .attr('id',(d => d.id))
+        .attr('d', path)
+        .attr('fill', (d => setColor(date, d.id)))
+    .append('title')
+        .text(d => d.id)
+    // gives state id as tooltip, need to change to name
+    });
+
+
+    const svg = select('svg')
+    .attr('width', 960)
+    .attr('height', 1000)
+
+    const path = geoPath()
+
+
 }
-
-const states = feature(topoJSONData, topoJSONData.objects.states)
-svg.selectAll('path').data(states.features)
-.enter().append('path')
-    .attr('class', 'state')
-    .attr('id',(d => d.id))
-    .attr('d', path)
-    .attr('fill', (d => setColor("AP20", d.id)))
-.append('title')
-    .text(d => d.id)
-// gives state id as tooltip, need to change to name
-});
-
-
-const svg = select('svg')
-.attr('width', 960)
-.attr('height', 1000)
-
-const path = geoPath()
-
-const colors = (code) => {
-    if (parseInt(code) >= 0 && parseInt(code) <= 25) {
-        return 'red'
-    } else {
-        return 'lightgreen'
-    }
-}
-
 
 
