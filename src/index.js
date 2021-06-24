5343248041d58d95673903217d03b0aa0ca6596f
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-24 12:50:43 -0700

    render by slider output

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './stylesheets/application.scss'
 import './stylesheets/map.scss'
 import './stylesheets/slider.scss'
+import { select, json, geoPath, tsv } from 'd3';
+
 import {getDates, dateCodes} from './dates'
 import { renderMap } from './map'
 
@@ -8,21 +10,6 @@ import { renderMap } from './map'
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    let slider = document.getElementById('range-slider')
-    let output = document.getElementById('value')
-
-    output.innerHTML = getDates[slider.value];
-
-    let date = dateCodes[slider.value]
-    renderMap(date)
-
-    slider.oninput = function() {
-        output.innerHTML = getDates[this.value];
-        date = dateCodes[this.value]
-        renderMap(date)
-    }
-
-
 
 
 })
--- src/map.js
+++ src/map.js
@@ -1,14 +1,49 @@
 import { select, json, geoPath, tsv } from 'd3';
 import { feature } from 'topojson-client';
+import {getDates, dateCodes} from './dates'
+let slider = document.getElementById('range-slider')
+let output = document.getElementById('value')
 
+slider.oninput = function() {
+    output.innerHTML = getDates[this.value];
+    d3.select("svg").selectAll("*")
+    // d3.select("svg").selectAll("*").remove();
+    // renderMap(dateCodes[this.value])
+}
 
-export const renderMap = (date) => {
     const jsonUrl = 'https://gist.githubusercontent.com/aedimoff/43582253126b56f90b942f80eee13156/raw/statesnumeric.json'
 
     Promise.all([
         json('https://d3js.org/us-10m.v1.json'),
         json(jsonUrl)
     ]).then(([topoJSONData, covidData]) => {
+
+    const svg = select('svg')
+    .attr('width', 960)
+    .attr('height', 1000)
+
+    const path = geoPath()
+
+    const states = feature(topoJSONData, topoJSONData.objects.states)
+    svg.selectAll('path').data(states.features)
+    .enter().append('path')
+        .attr('class', 'state')
+        .attr('id',(d => d.id))
+        .attr('d', path)
+        .attr('fill', (d => setColor(dateCodes[1], d.id)))
+    .append('title')
+        .text(d => d.id)
+    // gives state id as tooltip, need to change to name
+    
+
+
+    d3.selectAll("input").on("input", function change() {
+        const date = dateCodes[this.value];
+        console.log(date)
+        d3.selectAll("path").style("fill", function (d) {
+            return setColor(date, d.id)
+        })
+    })
 
     function getStatsByMonth(selectedMonth, stateId) {
         let stats;
@@ -22,6 +57,8 @@ export const renderMap = (date) => {
         })
         return stats;
     }
+
+ 
 
     function setColor(selectedMonth, stateId) {
         let stats = getStatsByMonth(selectedMonth, stateId)
@@ -39,26 +76,12 @@ export const renderMap = (date) => {
         
     }
 
-    const states = feature(topoJSONData, topoJSONData.objects.states)
-    svg.selectAll('path').data(states.features)
-    .enter().append('path')
-        .attr('class', 'state')
-        .attr('id',(d => d.id))
-        .attr('d', path)
-        .attr('fill', (d => setColor(date, d.id)))
-    .append('title')
-        .text(d => d.id)
-    // gives state id as tooltip, need to change to name
-    });
+});
 
 
-    const svg = select('svg')
-    .attr('width', 960)
-    .attr('height', 1000)
-
-    const path = geoPath()
 
 
-}
+
+
 
 
