919308a91eb555417bcb65d4e0c79369f8400ec3
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-24 09:57:01 -0700

    add slider

---
+++ src/dates.js
@@ -0,0 +1,33 @@
+export const getDates = ({
+    1 : "April 2020",
+    2 : "May 2020",
+    3 : "June 2020",
+    4 : "July 2020",
+    5 : "August 2020",
+    6 : "September 2020",
+    7 : "October 2020",
+    8 : "November 2020",
+    9 : "December 2020",
+    10 : "January 2021",
+    11 : "February 2021", 
+    12 : "March 2021", 
+    13 : "April 2021",
+    14 : "May 2021"
+})
+
+export const dateCodes = ({
+    1 : "AP20",
+    2 : "MY20",
+    3 : "JU20",
+    4 : "JY20",
+    5 : "AU20",
+    6 : "SE20",
+    7 : "OC20",
+    8 : "NO20",
+    9 : "DE20",
+    10 : "JA21",
+    11 : "FE21", 
+    12 : "MR21", 
+    13 : "AP21",
+    14 : "MY21"
+})
--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
 import * as Map from './map';
 import axios from "axios";
-import './stylesheets/style.scss'
+import './stylesheets/application.scss'
+import './stylesheets/map.scss'
+import './stylesheets/slider.scss'
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    
-    // axios.request({
-    //     method: 'GET',
-    //     url: 'https://covid-19-usa-data-by-zt.p.rapidapi.com/GetUSHistoricalDataBetweenDatesForState',
-    //     params: { end_date: '2021-01-30', start_date: '2021-01-01', statecode: 'CO' },
-    //     headers: {
-    //         'x-rapidapi-key': api_key,
-    //         'x-rapidapi-host': 'covid-19-usa-data-by-zt.p.rapidapi.com'
-    //     }
-    // })
-    // .then(function (response) {
-    //     console.log(response.data);
-    // }).catch(function (error) {
-    //     console.error(error);
-    // });
-    Map
 })
 
-// const options = (start = '2021-01-01', end = '2021-01-30', code = ) => ({
-//     method: 'GET',
-//     url: 'https://covid-19-usa-data-by-zt.p.rapidapi.com/GetUSHistoricalDataBetweenDatesForState',
-//     params: { end_date: `${end}`, start_date: `${start}`, statecode: `${code}` },
-//     headers: {
-//         'x-rapidapi-key': api_key,
-//         'x-rapidapi-host': 'covid-19-usa-data-by-zt.p.rapidapi.com'
-//     }
-// })
