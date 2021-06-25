030fcd91977b889121551b09711809a82704e2b5
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-25 08:37:02 -0700

    add styling and fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import './stylesheets/reset.scss'
 import './stylesheets/application.scss'
 import './stylesheets/map.scss'
 import './stylesheets/slider.scss'
+import axios from 'axios'
 import { select, json, geoPath, tsv } from 'd3';
 
 import {getDates, dateCodes} from './dates'
@@ -10,6 +12,50 @@ import { renderMap } from './map'
 
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    let slider = document.getElementById('range-slider')
+    let output = document.getElementById('value')
+
+
+    output.innerHTML = getDates[1]
+
+    slider.oninput = function() {
+        output.innerHTML = getDates[this.value];
+    }
+//     const requestNews = () => {   
+//     return new Promise((resolve, reject) => {
+//         axios.request({
+//             method: 'GET',
+//             url: 'https://covid-19-news.p.rapidapi.com/v1/covid',
+//             params: {
+//                 q: 'covid',
+//                 lang: 'en',
+//                 from: '2020/05/01',
+//                 to: '2020/05/31',
+//                 sort_by: 'relevancy',
+//                 sources: 'nytimes.com, cnn.com, forbes.com, usnews.com, abcnews.go.com, wsj.com, cbsnews.com, washingtonpost.com, time.com, newyorker.com, foxnews.com, bbc.com, theglobeandmail.com',
+//                 search_in: "title",
+//                 country: 'US',
+//                 media: 'True',
+//                 topic: "news"
+//             },
+//             headers: {
+//                 'x-rapidapi-key': api_key,
+//                 'x-rapidapi-host': 'covid-19-news.p.rapidapi.com'
+//             }
+//         }).then(response => {
+//             resolve(response.data)
+//         })
+//             .catch(error => {
+//                 console.error('auth.error', error);
+//                 reject(error)
+//             });
+//     });
+// };
+
+
+
+// requestNews().then(res => {console.log(res.articles)})
 
 
 })
--- src/map.js
+++ src/map.js
@@ -1,17 +1,8 @@
 import { select, json, geoPath, tsv } from 'd3';
 import { feature } from 'topojson-client';
+
 import {getDates, dateCodes} from './dates'
-let slider = document.getElementById('range-slider')
-let output = document.getElementById('value')
-
-slider.oninput = function() {
-    output.innerHTML = getDates[this.value];
-    d3.select("svg").selectAll("*")
-    // d3.select("svg").selectAll("*").remove();
-    // renderMap(dateCodes[this.value])
-}
-
-    const jsonUrl = 'https://gist.githubusercontent.com/aedimoff/43582253126b56f90b942f80eee13156/raw/statesnumeric.json'
+const jsonUrl = 'https://gist.githubusercontent.com/aedimoff/43582253126b56f90b942f80eee13156/raw/statesnumeric.json'
 
     Promise.all([
         json('https://d3js.org/us-10m.v1.json'),
@@ -19,8 +10,8 @@ slider.oninput = function() {
     ]).then(([topoJSONData, covidData]) => {
 
     const svg = select('svg')
-    .attr('width', 960)
-    .attr('height', 1000)
+    // .attr('width', 90vw)
+    // .attr('height', 1000)
 
     const path = geoPath()
 
@@ -62,16 +53,16 @@ slider.oninput = function() {
 
     function setColor(selectedMonth, stateId) {
         let stats = getStatsByMonth(selectedMonth, stateId)
-        if(stats < 1000) {
-            return "lightgreen"
-        } else if(stats > 1000 && stats <= 5000) {
-            return "lightblue"
+        if(stats < 5000) {
+            return "rgb(201, 207, 215)"
         } else if(stats > 5000 && stats <= 10000) {
-            return "yellow" 
-        } else if (stats > 10000 && stats <= 30000) {
-            return "orange"
-        } else if (stats > 30000) {
-            return "red"
+            return "rgb(246, 217, 176)"
+        } else if(stats > 10000 && stats <= 30000) {
+            return "rgb(254, 190, 114)" 
+        } else if (stats > 30000 && stats <= 100000) {
+            return "rgb(207, 104, 93)"
+        } else if (stats > 100000) {
+            return "rgb(149, 50, 49)"
         }
         
     }
