e94a79bdfdbed363b2b4d2e3cfa7c623b41a681b
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-27 20:27:02 -0700

    Add some logs

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
+console.log("**** process.env.NOD_ENV", process.env.NODE_ENV)
+console.log("**** process.env.NOD_ENV == production", process.env.NODE_ENV === 'production')
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_prod');
 } else {
