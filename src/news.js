067f15f4969f67dfeb08bd150ef43761862021d4
Arianne Dimoff <ariannedimoff@gmail.com>
2021-07-21 13:32:02 -0700

    readd original api

--- src/news.js
+++ src/news.js
@@ -1,64 +1,69 @@
 const api_key = require('../config/keys').API_KEY;
 
 export const getNews = (date) => {
-    //  let url = `https://covid-19-news.p.rapidapi.com/v1/covid?q=covid&lang=en&from=${date}-01&to=${date}-28&country=US&media=True`
-          let url = `https://newsapi.org/v2/top-headlines?q=covid&from=${date}-01&to=${date}-28&language=en&apiKey=${api_key}`;
-          fetch(url, {
-            method: "GET",
-          })
-            // fetch(url, {
-            //     "method": "GET",
-            //     "headers": {
-            //         "x-rapidapi-key": api_key,
-            //         "x-rapidapi-host": "covid-19-news.p.rapidapi.com"
-            //     }
-            // })
-            .then((response) => {
-              return response.json();
+     let url = `https://covid-19-news.p.rapidapi.com/v1/covid?q=covid&lang=en&from=${date}-01&to=${date}-28&country=US&media=True`
+
+            fetch(url, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": api_key,
+                    "x-rapidapi-host": "covid-19-news.p.rapidapi.com"
+                }
             })
-            .then((data) => {
-              //create array of 10 randomized articles
-              let articles = [];
-              while (articles.length < 10) {
-                let i = randomNum(data.articles.length);
-                articles.push(data.articles.splice(i, 1));
-              }
-              articles.flat().forEach((article) => {
-                  console.log("article", article)
-                //creates list item as container
-                let listItem = document.createElement("ul");
-                listItem.className = "news-item";
 
-                //creates img and adds image
-                let image = document.createElement("img");
-                image.className = "news-image";
-                image.src = `${article.urlToImage}`;
+              .then((response) => {
+                return response.json();
+              })
+              .then((data) => {
+                //create array of 10 randomized articles
+                let articles = [];
+                while (articles.length < 10) {
+                  let i = randomNum(data.articles.length);
+                  articles.push(data.articles.splice(i, 1));
+                }
+                articles.flat().forEach((article) => {
+                  console.log("article", article);
+                  //creates list item as container
+                  let listItem = document.createElement("ul");
+                  listItem.className = "news-item";
 
-                //creates h1 tag and inserts text
-                let header = document.createElement("h1");
-                header.className = "news-item-header";
-                let headertext = document.createTextNode(`${article.title}`);
-                header.appendChild(headertext);
+                  //creates img and adds image
+                  let image = document.createElement("img");
+                  image.className = "news-image";
+                  image.src = `${article.urlToImage}`;
 
-                //creates a tag and insterts text
-                let link = document.createElement("a");
-                link.className = "news-link";
-                let linktext = document.createTextNode(`Read Article`);
-                link.title = "Read Article";
-                link.href = `${article.link}`;
-                link.appendChild(linktext);
+                  //creates h1 tag and inserts headline text
+                  let header = document.createElement("h1");
+                  header.className = "news-item-header";
+                  let headertext = document.createTextNode(`${article.title}`);
+                  header.appendChild(headertext);
 
-                //apends header and link to list item
-                listItem.appendChild(image);
-                listItem.appendChild(header);
-                listItem.appendChild(link);
+                  //creates h3 tag and inserts publication date
+                  let pubDate = document.createElement("h3");
+                  pubDate.className = "news-item-date";
+                  let datetext = document.createTextNode("/public/news_image.jpg");
+                  pubDate.appendChild(datetext)
 
-                document.getElementById("news").appendChild(listItem);
+                  //creates a tag and insterts text
+                  let link = document.createElement("a");
+                  link.className = "news-link";
+                  let linktext = document.createTextNode(`Read Article`);
+                  link.title = "Read Article";
+                  link.href = `${article.link}`;
+                  link.appendChild(linktext);
+
+                  //apends header and link to list item
+                  listItem.appendChild(image);
+                  listItem.appendChild(header);
+                  listItem.appendChild(pubDate)
+                  listItem.appendChild(link);
+
+                  document.getElementById("news").appendChild(listItem);
+                });
+              })
+              .catch((error) => {
+                console.log(error);
               });
-            })
-            .catch((error) => {
-              console.log(error);
-            });
 
     }
 
