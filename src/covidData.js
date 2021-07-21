cbe52b05d90c336cda1cf1b48b15dcd9f60a6490
Arianne Dimoff <ariannedimoff@gmail.com>
2021-07-21 16:01:19 -0700

    add data for april2020

---
+++ src/covidData.js
@@ -0,0 +1,228 @@
+export const urls = [
+  "img-1",
+  "img-2",
+  "img-3",
+  "img-4",
+  "img-5",
+  "img-6",
+  "img-7",
+  "img-8",
+  "img-9",
+  "img-10",
+  "img-11",
+  "img-12",
+  "img-13",
+  "img-14",
+  "img-15",
+  "img-16",
+  "img-17",
+  "img-18",
+  "img-19",
+  "img-20",
+  "img-21",
+  "img-22",
+  "img-23",
+  "img-24",
+  "img-25",
+  "img-26",
+  "img-27",
+  "img-28",
+  "img-29",
+  "img-30",
+  "img-31",
+  "img-32",
+  "img-33",
+  "img-34",
+  "img-35",
+  "img-36",
+  "img-37",
+  "img-38",
+  "img-39",
+  "img-40",
+];
+
+export const april20 = {
+  articles: [
+    {
+      title: "The problem with Covid-19 death numbers",
+      published_date: "2020-04-30",
+      link: "https://www.cnn.com/2020/04/30/opinions/the-problem-with-covid-19-death-numbers-sutter/index.html",
+    },
+    {
+      title:
+        "Wuhan shows the world that the end of the lockdown is just the beginning of the Covid-19 crisis",
+      published_date: "2020-04-30",
+      link: "https://www.cnn.com/2020/04/29/asia/wuhan-coronavirus-lockdown-analysis-intl-hnk/index.html",
+    },
+    {
+      title:
+        "Debate emerges: Should businesses be protected from Covid-19 lawsuits?",
+      published_date: "2020-04-29",
+      link: "https://www.cnn.com/2020/04/29/business/business-liability-congress/index.html",
+    },
+    {
+      title: "Rare inflammatory syndrome seen in US child with Covid-19",
+      published_date: "2020-04-29",
+      link: "https://www.cnn.com/2020/04/29/health/rare-inflammatory-syndrome-us-covid-19/index.html",
+    },
+    {
+      title: "How New Zealand 'eliminated' Covid-19 after weeks of lockdown",
+      published_date: "2020-04-28",
+      link: "https://www.cnn.com/2020/04/28/asia/new-zealand-coronavirus-outbreak-elimination-intl-hnk/index.html",
+    },
+    {
+      title:
+        "Deaths spiked as Covid-19 spread in March and April, new analysis finds",
+      published_date: "2020-04-27",
+      link: "https://www.cnn.com/2020/04/27/health/deaths-spike-covid-spread/index.html",
+    },
+    {
+      title:
+        "New York hospitals are studying a common heartburn drug as treatment for Covid-19",
+      published_date: "2020-04-27",
+      link: "https://www.cnn.com/2020/04/27/health/famotidine-coronavirus-northwell-trial/index.html",
+    },
+    {
+      title: "Reviving America after Covid-19 will take years, not months",
+      published_date: "2020-04-26",
+      link: "https://www.cnn.com/2020/04/26/world/meanwhile-in-america-april-24/index.html",
+    },
+    {
+      title:
+        "Girl with rare blood disease inspires her peers to meet Covid-19 pandemic with hope, not fear",
+      published_date: "2020-04-25",
+      link: "https://www.cnn.com/2020/04/25/us/zoom-panel-kidscovid19-maryland/index.html",
+    },
+    {
+      title: "Researchers should study how Covid-19 affects women",
+      published_date: "2020-04-26",
+      link: "https://www.cnn.com/2020/04/25/opinions/coronavirus-research-women-blumenthal/index.html",
+    },
+    {
+      title:
+        "About a quarter of Covid-19 patients put on ventilators in New York's largest health system died, study finds",
+      published_date: "2020-04-26",
+      link: "https://www.cnn.com/2020/04/22/health/coronavirus-ventilator-patients-die/index.html",
+    },
+    {
+      title:
+        "2 studies show many people who tested positive for Covid-19 displayed no symptoms",
+      published_date: "2020-04-25",
+      link: "https://www.cnn.com/2020/04/24/us/2-asymptomatic-coronavirus-studies/index.html",
+    },
+    {
+      title:
+        "VA will continue to use unproven drug touted by Trump to treat Covid-19 despite FDA saying it has 'known risks'",
+      published_date: "2020-04-24",
+      link: "https://www.cnn.com/2020/04/24/politics/veterans-affairs-continue-hydroxychloroquine-treatment/index.html",
+    },
+    {
+      title: "Covid-19 is ravaging nursing homes. Government records show why",
+      published_date: "2020-04-24",
+      link: "https://www.cnn.com/2020/04/24/us/nursing-homes-coronavirus-invs/index.html",
+    },
+    {
+      title: "Wuhan weathered Covid-19. But can it survive what comes next?",
+      published_date: "2020-04-24",
+      link: "https://www.cnn.com/2020/04/24/asia/wuhan-coronavirus-business-life-intl-hnk/index.html",
+    },
+    {
+      title:
+        "House approves $480 billion package to help small businesses and hospitals, expand Covid-19 testing",
+      published_date: "2020-04-23",
+      link: "https://www.cnn.com/2020/04/23/politics/house-vote-small-business-aid-vote/index.html",
+    },
+    {
+      title: "The naive--and reckless--rule breakers of Covid-19",
+      published_date: "2020-04-23",
+      link: "https://www.cnn.com/2020/04/23/opinions/covid-19-secret-rule-breakers/index.html",
+    },
+    {
+      title:
+        "NIH Covid-19 treatment guidelines warn against drug combo pushed by Trump",
+      published_date: "2020-04-21",
+      link: "https://www.cnn.com/2020/04/21/health/nih-covid-19-treatment-guidelines/index.html",
+    },
+    {
+      title:
+        "Standoff continues over small business relief as Democrats push for national Covid-19 testing strategy",
+      published_date: "2020-04-20",
+      link: "https://www.cnn.com/2020/04/20/politics/congress-sba-negotiations/index.html",
+    },
+    {
+      title:
+        "Covid's medical devastation will be felt by millions who never contract the disease",
+      published_date: "2020-04-21",
+      link: "https://www.cnn.com/2020/04/21/opinions/medical-cost-of-covid-19-is-bigger-than-virus-chang-barry/index.html",
+    },
+    {
+      title:
+        "A deep craving for 'normalcy' goes up against the reality of Covid-19's deadly risk",
+      published_date: "2020-04-17",
+      link: "https://www.cnn.com/2020/04/17/opinions/return-to-normalcy-deadly-covid-19-risk-hemmer/index.html",
+    },
+    {
+      title:
+        "Covid-19 antibody tests can help people rejoin society, but some are stuck in China",
+      published_date: "2020-04-17",
+      link: "https://www.cnn.com/2020/04/17/politics/covid-19-antibody-tests-china/index.html",
+    },
+    {
+      title:
+        "States finally have high-speed machines to detect Covid-19 -- but few tests to run on them",
+      published_date: "2020-04-15",
+      link: "https://www.cnn.com/2020/04/15/politics/coronavirus-rapid-testing-states-abbott-machines-few-tests-invs/index.html",
+    },
+    {
+      title:
+        "CDC estimates more than 9,200 health care workers have been infected with covid-19",
+      published_date: "2020-04-15",
+      link: "https://www.cnn.com/2020/04/15/health/coronavirus-9200-health-workers-infected/index.html",
+    },
+    {
+      title: "FDA tightens enforcement on Covid-19 antibody tests",
+      published_date: "2020-04-14",
+      link: "https://www.cnn.com/2020/04/14/health/fda-reverses-antibody-tests/index.html",
+    },
+    {
+      title:
+        "'Such a simple thing to do': Why positioning Covid-19 patients on their stomachs can save lives",
+      published_date: "2020-04-14",
+      link: "https://www.cnn.com/2020/04/14/health/coronavirus-prone-positioning/index.html",
+    },
+    {
+      title:
+        "Model predicts Covid-19 pandemic will 'peter out' by May, but experts are skeptical",
+      published_date: "2020-04-13",
+      link: "https://www.cnn.com/2020/04/13/health/us-coronavirus-updates-monday/index.html",
+    },
+    {
+      title: "Why the US has the world's highest number of Covid-19 deaths",
+      published_date: "2020-04-12",
+      link: "https://www.cnn.com/2020/04/12/opinions/coronavirus-us-death-toll-trump-sachs-opinion/index.html",
+    },
+    {
+      title:
+        "Study finds no benefit, higher death rate in patients taking hydroxychloroquine for Covid-19",
+      published_date: "2020-04-21",
+      link: "https://www.cnn.com/2020/04/21/health/hydroxychloroquine-veterans-study/index.html",
+    },
+    {
+      title:
+        "Doctors try to untangle why they're seeing 'unprecedented' blood clotting among Covid-19 patients",
+      published_date: "2020-04-22",
+      link: "https://www.cnn.com/2020/04/21/health/blood-clots-coronavirus/index.html",
+    },
+    {
+      title: "Seniors with Covid-19 show unusual symptoms, doctors say",
+      published_date: "2020-04-23",
+      link: "https://www.cnn.com/2020/04/23/health/seniors-elderly-coronavirus-symptoms-wellness-partner/index.html",
+    },
+    {
+      title:
+        "Covid-19 vaccine trial on humans starts as UK warns restrictions could stay in place until next year",
+      published_date: "2020-04-23",
+      link: "https://www.cnn.com/2020/04/23/health/coronavirus-vaccine-trial-uk-gbr-intl/index.html",
+    },
+  ],
+};
--- src/news.js
+++ src/news.js
@@ -1,6 +1,10 @@
 const api_key = require("../config/keys").API_KEY;
+import { urls, april20 } from "./covidData";
 
 export const getNews = (date) => {
+  if (date === "2020-04") {
+    return createNewsElements(april20);
+  }
   let url = `https://covid-19-news.p.rapidapi.com/v1/covid?q=covid&lang=en&from=${date}-01&to=${date}-28&country=US&media=True`;
 
   fetch(url, {
@@ -14,107 +18,71 @@ export const getNews = (date) => {
       return response.json();
     })
     .then((data) => {
-      //create array of 10 randomized articles
-      let articles = [];
-      while (articles.length < 10) {
-        let i = randomNum(data.articles.length);
-        articles.push(data.articles.splice(i, 1));
-      }
-      articles.flat().forEach((article) => {
-        //creates list item as container
-        let listItem = document.createElement("ul");
-        listItem.className = "news-item";
-
-        //creates img and adds image
-        let image = document.createElement("img");
-        let images = urls
-        let idx = randomNum(images.length)
-        let src = images.splice(idx, 1)
-        image.className = "news-image";
-        image.src = `${src[0].toString()}.jpg`;
-
-        //creates h1 tag and inserts headline text
-        let header = document.createElement("h1");
-        header.className = "news-item-header";
-        let headertext = document.createTextNode(article.title);
-        header.appendChild(headertext);
-
-        //creates h3 tag and inserts publication date
-        let pubDate = document.createElement("h4");
-        pubDate.className = "news-item-date";
-        let date = new Date(article.published_date.split(" ")[0]);
-        let dateString = date.toString("MMMM dS, yyyy").split(" 17:00:00")[0];
-
-        let datetext = document.createTextNode(
-          "Published: " + dateString.split(" ").slice(1).join(" ")
-        );
-        pubDate.appendChild(datetext);
-
-        //creates a tag and insterts text
-        let link = document.createElement("a");
-        link.className = "news-link";
-        let linktext = document.createTextNode(`Read Article`);
-        link.title = "Read Article";
-        link.href = article.link;
-        link.appendChild(linktext);
-
-        //apends header and link to list item
-        listItem.appendChild(image);
-        listItem.appendChild(header);
-        listItem.appendChild(pubDate);
-        listItem.appendChild(link);
-
-        document.getElementById("news").appendChild(listItem);
-      });
+      createNewsElements(data);
     })
     .catch((error) => {
       console.log(error);
     });
 };
 
+function createNewsElements(data) {
+  let articles = [];
+  while (articles.length < 10) {
+    let i = randomNum(data.articles.length);
+    articles.push(data.articles.splice(i, 1));
+  }
+  articles.flat().forEach((article) => {
+    //creates list item as container
+    let listItem = document.createElement("ul");
+    listItem.className = "news-item";
+
+    //creates img and adds image
+    let image = document.createElement("img");
+    let images = urls;
+    let idx = randomNum(images.length);
+    let src = images.splice(idx, 1);
+    image.className = "news-image";
+    image.src = `${src[0].toString()}.jpg`;
+
+    //creates h1 tag and inserts headline text
+    let header = document.createElement("h1");
+    header.className = "news-item-header";
+    let headertext = document.createTextNode(article.title);
+    header.appendChild(headertext);
+
+    //creates h3 tag and inserts publication date
+    let pubDate = document.createElement("h4");
+    pubDate.className = "news-item-date";
+    let date = new Date(article.published_date.split(" ")[0]);
+    let dateString = date.toString("MMMM dS, yyyy").split(" 17:00:00")[0];
+
+    let datetext = document.createTextNode(
+      "Published: " + dateString.split(" ").slice(1).join(" ")
+    );
+    pubDate.appendChild(datetext);
+
+    //creates a tag and insterts text
+    let link = document.createElement("a");
+    link.className = "news-link";
+    let linktext = document.createTextNode(`Read Article`);
+    link.title = "Read Article";
+    link.href = article.link;
+    link.appendChild(linktext);
+
+    //appends header and link to list item
+    listItem.appendChild(image);
+    listItem.appendChild(header);
+    listItem.appendChild(pubDate);
+    listItem.appendChild(link);
+
+    document.getElementById("news").appendChild(listItem);
+  });
+}
+
 function randomNum(size) {
   return Math.floor(Math.random() * size);
 }
 
-let urls = [
-  "img-1",
-  "img-2",
-  "img-3",
-  "img-4",
-  "img-5",
-  "img-6",
-  "img-7",
-  "img-8",
-  "img-9",
-  "img-10",
-  "img-11",
-  "img-12",
-  "img-13",
-  "img-14",
-  "img-15",
-  "img-16",
-  "img-17",
-  "img-18",
-  "img-19",
-  "img-20",
-  "img-21",
-  "img-22",
-  "img-23",
-  "img-24",
-  "img-25",
-  "img-26",
-  "img-27",
-  "img-28",
-  "img-29",
-  "img-30",
-  "img-31",
-  "img-32",
-  "img-33",
-  "img-34",
-  "img-35",
-  "img-36",
-  "img-37",
-  "img-38",
-  "img-39",
-  "img-40",
-];
+
+
+
